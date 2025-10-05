59d2cdf2f37edcaeadcb55ae566688c636f56f16
Kevin Puscán Ortiz <kevinjpuscan@gmail.com>
2025-10-05 14:48:52 -0500

    feat: add multiprocess

--- src/services/data_processor_service.py
+++ src/services/data_processor_service.py
@@ -16,15 +16,19 @@ def process_data():
 
     no2_data = earthdata.fetch_no2_data()
     alert_zones_no2 = alert_zones.detect(no2_data)
+    grouped_alert_zones_no2 = group_data.group_all_alert_zones("NO2", alert_zones_no2, datetime)
     no2_grouped_info = group_data.group_data("NO2", no2_data, datetime)
     storage.save_files(no2_grouped_info)
+    storage.save_files(grouped_alert_zones_no2)
 
     hcho_data = earthdata.fetch_hcho_data()
     alert_zones_hcho = alert_zones.detect(hcho_data, umbral=4e16)
+    grouped_alert_zones_hcho = group_data.group_all_alert_zones("HCHO", alert_zones_hcho, datetime)
     hcho_grouped_info = group_data.group_data("HCHO", hcho_data, datetime)
     storage.save_files(hcho_grouped_info)
+    storage.save_files(grouped_alert_zones_hcho)
 
-    mongo.save_processing(datetime)
+    db.save_processing(datetime)
 
     data = {
         "no2": len(no2_data),
@@ -43,3 +47,21 @@ def save_fire_data():
     storage.save_single_file(
         "LANDSAT_USA_contiguous_and_Hawaii_24h.csv", response.content
     )
+
+
+def historical_data(date):
+    
+    no2_data = earthdata.fetch_no2_historical_data_warnings(date)
+    historical_no2 = group_data.group_historical_data(no2_data, "NO2", date)
+    storage.save_files(historical_no2)
+
+    hcho_data = earthdata.fetch_hcho_historical_data_warnings(date)
+    historical_hcho = group_data.group_historical_data(hcho_data, "HCHO", date)
+    storage.save_files(historical_hcho)
+
+    db.save_processing(date, process_key="historical_day_warning_points")
+
+    return {
+        "no2": len(no2_data),
+        "hcho": len(hcho_data),
+    }
--- src/utils/storage.py
+++ src/utils/storage.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 from botocore.exceptions import ClientError
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from src.utils.logger import logger
 import src.config as config
 
@@ -10,12 +11,14 @@ AWS_REGION = config.AWS_REGION
 AWS_BUCKET_NAME= config.AWS_BUCKET_NAME
 
 
+WORKERS = 64
+
 def _upload_json(data, bucket_name, object_key):
     s3 = boto3.client(
         's3',
         aws_access_key_id=AWS_ACCESS_KEY_ID,
         aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=AWS_REGION
+        region_name=AWS_REGION,
     )
     try:
         json_bytes = json.dumps(data).encode('utf-8')
@@ -32,15 +35,25 @@ def _upload_json(data, bucket_name, object_key):
         return False
 
 def save_files(files, bucket_name=AWS_BUCKET_NAME):
-    for file in files:
+    def upload_one(file):
         filename = file['filename']
         content = file['content']
         object_key = filename
-        success = _upload_json(content, bucket_name, object_key)
-        if success:
+        result = _upload_json(content, bucket_name, object_key)
+        if result:
             logger.info(f"Archivo {filename} subido exitosamente a {bucket_name}/{object_key}")
+            return filename
         else:
             logger.error(f"Error subiendo el archivo {filename} a {bucket_name}/{object_key}")
+            raise Exception(f"Error subiendo {filename}")
+
+    with ThreadPoolExecutor(max_workers=min(WORKERS, len(files))) as ex:
+        futs = [ex.submit(upload_one, file) for file in files]
+        for f in as_completed(futs):
+            try:
+                f.result()
+            except Exception as e:
+                logger.error(f"FAIL: {e}")
 
 def save_single_file(key, content, bucket_name=AWS_BUCKET_NAME):
     s3 = boto3.client(
@@ -59,3 +72,4 @@ def save_single_file(key, content, bucket_name=AWS_BUCKET_NAME):
     )
 
     logger.info(f'Archivo guardado en s3://{bucket_name}/{key}')
+
